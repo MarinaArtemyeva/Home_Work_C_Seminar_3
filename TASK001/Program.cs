389e0e64a50065b7ce29442857e7dfe7df0dca65
Marina Artemyeva <martemyeva@yahoo.com>
2023-02-19 11:00:52 +0600

    Add code  TASK001

--- TASK001/Program.cs
+++ TASK001/Program.cs
@@ -1,2 +1,33 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая принимает на вход 
+// пятизначное число и проверяет, является ли оно палиндромом.
+// 14212 -> нет
+// 12821 -> да
+// 23432 -> да
+
+void Palidrom()
+{
+    Console.WriteLine("Введите пятизначное число: ");
+    int number = Convert.ToInt32(Console.ReadLine());
+    int num1 = number / 10000 % 10;
+    int num2 = number / 1000 % 10;
+    int rev1 = number / 10 % 10;
+    int rev2 = number % 10;
+    int nul = number / 10000;
+
+    if (nul < 1 || nul > 9)
+    {
+        Console.WriteLine(number + " -> " + "нет");
+    }
+    else if (num1 == rev2 && num2 == rev1)
+    {
+        Console.WriteLine(number + " -> " + "да");
+    }
+    else
+    {
+        Console.WriteLine(number + " -> " + "нет");
+    }
+}
+for (int index = 0; index < 10; index++)
+{
+    Palidrom();
+}
