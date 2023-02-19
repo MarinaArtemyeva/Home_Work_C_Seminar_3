e518acb8f85de52e9362e839f2606b9b088bc866
Marina Artemyeva <martemyeva@yahoo.com>
2023-02-19 15:36:10 +0600

    Add code  TASK002

--- TASK002/Program.cs
+++ TASK002/Program.cs
@@ -3,3 +3,27 @@
 // ними в 3D пространстве.
 // A (3,6,8); B (2,1,-7), -> 15.84
 // A (7,-5, 0); B (1,-1,9) -> 11.53
+
+int x1 = Coordinate("x", "A");
+int y2 = Coordinate("y", "A");
+int z3 = Coordinate("z", "A");
+int x1 = Coordinate("x", "B");
+int y2 = Coordinate("y", "B");
+int z3 = Coordinate("z", "B");
+
+int Coordinate(string corName, string pointName)
+{
+    Console.Write("Введите координату" (corName), "точки" (pointName), ":");
+    return Convert.ToInt16(Console.ReadLine());
+}
+double Decision(double x1, double x2, 
+                double y1, double y2, 
+                double z1, double z2)
+{
+    return Math.Sqrt((Math.Pow(x2 - x1), 2) + 
+          (Math.Pow(y2 - y1), 2) + 
+          (Math.Pow(z2 - z1), 2));
+}
+double segmentLength = Math.Round (Decision(x1, x2, y1, y2, z1, z1), 2);
+
+Console.WriteLine("Длина отрезка: ", segmentLength);
